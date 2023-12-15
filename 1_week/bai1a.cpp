0a6ee3581ff8c8764aca2e1892217df8bc81180c
NguyenHoanKhanh <khanhvlg12022004@gmail.com>
2023-12-15 14:15:21 +0700

    New

--- 1_week/bai1a.cpp
+++ 1_week/bai1a.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void linearsort(int n, int arr[], int find)
+void linearsort(int n, vector<int>&a, int find)
 {
     bool check = false;
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] == find)
+        if (a[i] == find)
         {
             cout<<"Phan tu "<<find<<" duoc tim thay o : "<<i+1;
         }
@@ -24,17 +24,17 @@ int main()
     cout<<"Nhap vao kich thuoc mang : ";
     cin>>n;
 
-    int* arr = new int[n];
+    vector<int>a(n);
     for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin>>a[i];
     }
     
     int find;
     cout<<"nhap vao gia tri can tim trong mang : ";
     cin>>find;
 
-    linearsort(n, arr, find);
+    linearsort(n, a, find);
 
     return 0;
 }
